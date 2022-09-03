30cc2a798959f7b35d2cb81865f3a4e7f3718535
Marcos Palacios <mp2772@gmail.com>
2022-09-03 11:56:55 -0400

    updated express intro

--- express-intro/app.js
+++ express-intro/app.js
@@ -1,10 +1,36 @@
 const express = require('express');
 const app = express();
+const PORT = 3000;
+// variables and declarations always at the top of the file
+
+
+
+// middleware always comes between declarations and routes
+// ========== MIDDLEWARE =============
+app.use(express.static('public'));
+app.use(express.static('views'));
+
+
+// ===================================
+
+
+// routes are the 2nd to last element in your app.js
+// ============== ROUTES =====================
 
 app.get('/', (req, res, next) => {
-    
+    res.sendFile(__dirname + '/views/index.html');
 })
 
-app.post('/post', (req, res, next) => {
-
+app.get('/about', (req, res, next) => {
+    res.sendFile(__dirname + '/views/about.html');
 })
+
+app.get('/contact', (req, res, next) => {
+    res.sendFile(__dirname + '/views/contact.html');
+})
+
+// ============================================
+
+
+// remember to listen at bottom of the app js
+app.listen(PORT || 3000, () => console.log(`Listening on port ${PORT}`));
