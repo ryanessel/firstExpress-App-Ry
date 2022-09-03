bd1b7e9d4b43cf30851f32be5ed2749423880531
Marcos Palacios <mp2772@gmail.com>
2022-09-03 16:25:07 -0400

    added layout and hbs example to express introl

--- express-intro/app.js
+++ express-intro/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const axios = require('axios');
 // variables and declarations always at the top of the file
 
 
@@ -8,8 +9,9 @@ const PORT = 3000;
 // middleware always comes between declarations and routes
 // ========== MIDDLEWARE =============
 app.use(express.static('public'));
-app.use(express.static('views'));
-
+// app.use(express.static('views'));
+app.set("views", __dirname + "/views");
+app.set("view engine", "hbs");
 
 // ===================================
 
@@ -18,15 +20,46 @@ app.use(express.static('views'));
 // ============== ROUTES =====================
 
 app.get('/', (req, res, next) => {
-    res.sendFile(__dirname + '/views/index.html');
+    // this is how you display a file without hbs as a view engine
+    // res.sendFile(__dirname + '/views/index.html');
+
+    const pageName = 'Home Screen';
+    const displayArray = ["Hats", "Dogs", "Reptiles", "Phones", 'Cars', "Nails"];
+
+    const blah = {
+        pageName,
+        content: displayArray,
+        showContent: displayArray.length > 5
+    }
+
+    // this is how you render a file having a view engine like "hbs"
+    // res.render('index', {pageName, content: displayArray, showContent: displayArray.length > 5});
+    res.render('index', blah);
+
+
+    // when your express side is an api only, then you just have to res.json the inforamtion
+    // res.json({pageName, content: displayArray});
 })
 
 app.get('/about', (req, res, next) => {
-    res.sendFile(__dirname + '/views/about.html');
+    // res.sendFile(__dirname + '/views/about.html');
+    axios.get('https://pokeapi.co/api/v2/pokemon').then(apiRes => {
+        console.log({apiRes: apiRes.data.results});
+
+        const data = {
+            content: apiRes.data.results
+        }
+        res.render('about', data);
+    }).catch(err => {
+        console.log({err});
+        next();
+    })
+    // res.render('about');
 })
 
 app.get('/contact', (req, res, next) => {
-    res.sendFile(__dirname + '/views/contact.html');
+    // res.sendFile(__dirname + '/views/contact.html');
+    res.render('contact');
 })
 
 // ============================================
