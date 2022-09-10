9d4b3f3e689e2b32871e6ce7c1d4cd3fc55f675b
Ryan Essel <spucti19@gmail.com>
2022-09-10 12:31:22 -0400

    set up template

--- express-app-al-ry/app.js
+++ express-app-al-ry/app.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
+const mongoose = require ("mongoose");
 const PORT = 3000;
-const axios = require('axios');
+// const axios = require('axios');
 // variables and declarations always at the top of the file
 
-
+// ====================connection to db =============
+mongoose                        //   |
+  .connect('mongodb://localhost/expressApp')
+  .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
+  .catch(err => console.error('Error connecting to mongo', err));
+//===================================================
 
 // middleware always comes between declarations and routes
 // ========== MIDDLEWARE =============
@@ -16,54 +22,73 @@ app.set("view engine", "hbs");
 // ===================================
 
 
-// routes are the 2nd to last element in your app.js
-// ============== ROUTES =====================
 
-app.get('/', (req, res, next) => {
-    // this is how you display a file without hbs as a view engine
-    // res.sendFile(__dirname + '/views/index.html');
+//================ROUTES========================
 
-    const pageName = 'Home Screen';
-    const displayArray = ["Hats", "Dogs", "Reptiles", "Phones", 'Cars', "Nails"];
-
-    const blah = {
-        pageName,
-        content: displayArray,
-        showContent: displayArray.length > 5
-    }
-
-    // this is how you render a file having a view engine like "hbs"
-    // res.render('index', {pageName, content: displayArray, showContent: displayArray.length > 5});
-    res.render('index', blah);
+//telling app to use this file-- what determins routes.
+// if you add /blah then all the routes in you r index file must start with /blach before any route is defined. ie: you create a route in index.js that has an endpoint of /home but you prefixed /blah in the app.js to require index.js your end result of the route would be  www.domainName.com/blah/home
+    app.use(`/`, require (`./routes/index`));
 
 
-    // when your express side is an api only, then you just have to res.json the inforamtion
-    // res.json({pageName, content: displayArray});
-})
 
-app.get('/about', (req, res, next) => {
-    // res.sendFile(__dirname + '/views/about.html');
-    axios.get('https://pokeapi.co/api/v2/pokemon').then(apiRes => {
-        console.log({apiRes: apiRes.data.results});
 
-        const data = {
-            content: apiRes.data.results
-        }
-        res.render('about', data);
-    }).catch(err => {
-        console.log({err});
-        next();
-    })
-    // res.render('about');
-})
 
-app.get('/contact', (req, res, next) => {
-    // res.sendFile(__dirname + '/views/contact.html');
-    res.render('contact');
-})
 
 // ============================================
 
 
 // remember to listen at bottom of the app js
 app.listen(PORT || 3000, () => console.log(`Listening on port ${PORT}`));
+
+
+
+
+
+
+
+
+// // routes are the 2nd to last element in your app.js
+// // ============== ROUTES =====================
+
+// app.get('/', (req, res, next) => {
+//     // this is how you display a file without hbs as a view engine
+//     // res.sendFile(__dirname + '/views/index.html');
+
+//     const pageName = 'Home Screen';
+//     const displayArray = ["Hats", "Dogs", "Reptiles", "Phones", 'Cars', "Nails"];
+
+//     const blah = {
+//         pageName,
+//         content: displayArray,
+//         showContent: displayArray.length > 5
+//     }
+
+//     // this is how you render a file having a view engine like "hbs"
+//     // res.render('index', {pageName, content: displayArray, showContent: displayArray.length > 5});
+//     res.render('index', blah);
+
+
+//     // when your express side is an api only, then you just have to res.json the inforamtion
+//     // res.json({pageName, content: displayArray});
+// })
+
+// app.get('/about', (req, res, next) => {
+//     // res.sendFile(__dirname + '/views/about.html');
+//     axios.get('https://pokeapi.co/api/v2/pokemon').then(apiRes => {
+//         console.log({apiRes: apiRes.data.results});
+
+//         const data = {
+//             content: apiRes.data.results
+//         }
+//         res.render('about', data);
+//     }).catch(err => {
+//         console.log({err});
+//         next();
+//     })
+//     // res.render('about');
+// })
+
+// app.get('/contact', (req, res, next) => {
+//     // res.sendFile(__dirname + '/views/contact.html');
+//     res.render('contact');
+// })
---
+++ express-app-al-ry/public/scripts/script.js
@@ -0,0 +1,3 @@
+window.addEventListener(`load`, () => {
+    console.log("hellow is this working? if you see this it isss")
+})
